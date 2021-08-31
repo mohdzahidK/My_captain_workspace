a50e65e7e169d8907ef961d3cd1420c850fbc0af
MohammedZahid K <74446928+mohdzahidK@users.noreply.github.com>
2021-08-31 08:43:48 +0530

    1

---
+++ Java/java tutorial/JavaApp/src/Name.java
@@ -0,0 +1,20 @@
+public class Name {
+	import java.util.Scanner;
+
+	public class Nm {
+		public static void main(String[] args) {
+		//String name = new String();
+		//int arr =new arr[[10]]
+		 System.out.println("enter a name");
+		 String name = new String()
+		 Scanner s = new Scanner(System.in);
+		 name = s.nextLine();
+		 //Scanner t = new Scanner(System.in);
+		 System.out.println(name);
+		 
+		 
+		}
+
+	}
+
+}
---
+++ Java/java tutorial/javaapplication/src/javaapplication/Test.java
@@ -0,0 +1,13 @@
+package javaapplication;
+
+public class Test {
+	int x=10;
+	int y=5;
+	public static void main(string[] args) {
+		Test obj1 = new Test();
+		Test obj2 = new Test();
+		System.out.println("test"+obj1.x);
+		System.out.println("test"+obj1.y);
+	}
+
+}
